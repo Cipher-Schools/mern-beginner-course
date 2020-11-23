9a2edead5362dfdb1c7892360fdde513d194e6d6
Palash <palash.tripathi@gmail.com>
2020-11-23 21:46:46 +0530

    Javascript Datatypes

---
+++ Javascript/example.js
@@ -0,0 +1,99 @@
+// Datatypes
+
+// Syntax and declaration
+
+// var score = 10;  // ES5 - Ecma 
+
+// let result = 50; // ES6 - Ecmascript 2015
+// const PI = 3.14;  // ES6 
+
+
+// score = 15;
+// result = 55;
+// PI = 3.26;
+
+// console.log(score);
+// console.log(result);
+// console.log(PI);
+
+
+/**
+ *  Datatypes
+ *  1. Number
+ *  2. Strings
+ *  3. Boolean
+ *  4. Null
+ *  5. Undefined
+ *  6. Objects
+ *  7. Symbols (es6)
+ * 
+ *  Loosely Typed
+ */
+
+// let score = 10; // Number
+// let result = '10'; // String
+
+// BEDMAS  (Bracket Exponent Divison Multiplication addition Substraction)
+    // let finalScore = score + result; // Concatenating with string
+    // let penalty = score - result; // Mathematical
+    // let totalGames = 'five';
+    // // let avgScore = finalScore / totalGames;
+
+    // // console.log(finalScore, penalty);
+    // // console.log(avgScore);
+    // // // typeof
+    // // console.log(typeof finalScore);
+    // // console.log(typeof penalty);
+    // // console.log(typeof avgScore);
+    // let value = (finalScore**2)*10/(penalty + totalGames)
+
+    // console.log(penalty * totalGames);
+
+    // console.log(value); // Value = NaN (not a number)
+
+
+// Strings (String and string)
+
+    // let number = "raining"; // String.prototype 
+    // let string = "String as a name"
+
+    // let String= 20083838;
+
+    // // console.log(word.toUpperCase());
+    // console.log(String);
+
+    // console.log('hello and good evening'.length)
+    // console.log("hello".charAt(4))
+
+// Boolean (true, 1, false, 0)
+
+    // console.log(typeof true);
+    // console.log(typeof false);
+    // console.log(isNaN('five'));
+
+    // Truthy Values: any value 
+    // Falsy Values: 0 false '' NaN undefined null
+
+    // let condition = 10/'hello'; // NaN
+    // if(condition){
+    //     console.log("Very true!!")
+    // }else{
+    //     console.log("very wrong!!")
+    // }
+
+    // console.log('hello'.includes('e'));
+
+
+// Null Undefined 
+    // let score;
+    // let result = null;
+
+    //     if(result){
+    //         console.log("Very true!!")
+    //     }else{
+    //         console.log("very wrong!!")
+    //     }
+
+    // console.log(typeof score);
+    // console.log(typeof result);
+
